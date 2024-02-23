4c6f0ce5e89c04ac1f75e84b3ce29f695a42f7b3
SDG-Ebenezer <99293985+SDG-Ebenezer@users.noreply.github.com>
2024-02-22 19:57:04 -0500

    fix inventory switching bug

--- app.js
+++ app.js
@@ -476,6 +476,7 @@ io.sockets.on("connection", (socket)=>{
     //player drop item
     socket.on("drop", (data)=>{
         let player = entities[id]
+        player.invSelected = data.playerInvI // update now! why not?
         let x = data.x
         let y = data.y
         //see if out of boundaries
